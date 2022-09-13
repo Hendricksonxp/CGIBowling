add5f49e20f0973d05f266952f85b64498f900ac
Chet Hendrickson <chet@hendricksonxp.com>
2022-09-13 12:31:39 -0400

    finished

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,5 +1,7 @@
 public class BowlingGame {
+    public static final int NumberOfFrames = 10;
     private final int[] rolls;
+    private int rollIndex = 0;
 
     public BowlingGame(int[] rolls) {
         this.rolls = rolls;
@@ -7,20 +9,35 @@ public class BowlingGame {
 
     public int score() {
         int result = 0;
-        int rollIndex = 0;
-        for (int i = 0; i < 10; i++) {
-            if (rolls[rollIndex] == 10){
-                result += rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+        for (int i = 0; i < NumberOfFrames; i++) {
+            if (isStrike()){
+                result += 10 + nextRoll() + nextNextRoll();
                 rollIndex += 1;
-            } else if(rolls[rollIndex] + rolls[rollIndex + 1] == 10) {
-                result += rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+            } else if(isSpare()) {
+                result += 10 + nextNextRoll();
                 rollIndex += 2;
             } else {
-                result += rolls[rollIndex] + rolls[rollIndex + 1];
+                result += rolls[rollIndex] + nextRoll();
                 rollIndex += 2;
             }
 
         }
         return result;
+    }
+
+    private int nextNextRoll() {
+        return rolls[rollIndex + 2];
+    }
+
+    private int nextRoll() {
+        return rolls[rollIndex + 1];
+    }
+
+    private boolean isSpare() {
+        return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+    }
+
+    private boolean isStrike() {
+        return rolls[rollIndex] == 10;
     }
 }
